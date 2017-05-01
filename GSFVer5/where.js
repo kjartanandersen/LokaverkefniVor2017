3e4bd432df7022a73332003059966acd0fa141d0
kjartanandersen <svidakjammi@gmail.com>
2017-05-01 01:31:39 +0000

    Added GSF

---
+++ GSFVer5/where.js
@@ -0,0 +1,8 @@
+myData = [
+[59.393847,24.6650872, 'TTÜ staadion, 12616 Tallinn, Estonia'],
+[58.3733281,26.7265098, 'Tartu Ülikooli Füüsikahoone, 50103 Tartu, Estonia'],
+[33.6778327,-117.8151285, 'Padua, Irvine, CA 92614, USA'],
+[18.5544976,73.8257325, 'Pune University, Ganeshkhind, Pune, Maharashtra, India'],
+[37.8805941,-122.2447958, 'Space Sciences Laboratory at University of California, 7 Gauss Way, Berkeley, CA 94720, USA'],
+[43.0765915,-89.4052247, 'William H. Sewell Social Sciences Building, 1180 Observatory Dr, Madison, WI 53706, USA']
+];
